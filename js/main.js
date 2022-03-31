eb11e669ebc5f4ed680b62f231fbf6e656e0cf93
Owen Adams <owenadams.main@gmail.com>
2022-03-31 15:34:31 -0400

    query the dom for form element. add event listeners to update placeholder image src and create object. #1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
 /* global data */
 /* exported data */
+
+const $inputForm = document.querySelector('#entry-form');
+const $imageHolder = document.querySelector('.journal-image');
+
+$inputForm.addEventListener('input', event => {
+  if (event.target.getAttribute('id') === 'image-url') {
+    $imageHolder.setAttribute('src', event.target.value);
+  }
+});
+
+$inputForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const journalEntry = {
+    title: event.target[0].value,
+    photoUrl: event.target[1].value,
+    notes: event.target[2].value,
+    entryId: data.nextEntryId++
+  };
+  // eslint-disable-next-line
+  console.log(journalEntry);
+});
